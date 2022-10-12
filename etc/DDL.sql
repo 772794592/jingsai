16439fb2683869016038068fe48a0ddb9aefcb3b
772794592 <772794592@qq.com>
2022-10-12 16:04:05 +0800

    Use 50.190 as the data source databases: ferryway username:root pswd:123456

---
+++ etc/DDL.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS ferryway DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+USE ferryway;
+
+DROP TABLE IF EXISTS `t_user` CASCADE;
+
+
+
+CREATE TABLE `t_user`(
+ `id` INT(11) NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+ `username` VARCHAR (32) NOT NULL COMMENT '名字',
+ `age` INT(3) NOT NULL COMMENT '年龄'
+)ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+INSERT INTO t_user(`username`,`age`) values('张三','20');
+
